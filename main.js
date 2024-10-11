6796cc35ca5a667a0a4d955f76ead10fd6eb1b2b
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-11 12:12:15 -0300

    ,

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function criaCartao (categoria, pergunta, resposta) {
     <p>${pergunta}</p>
   </div>
   <div class="resposta-cartao"></div>
-  <p>${pergunta}</p>
+  <p>${resposta}</p>
 </div>`
 conteiner.appendChild(cartao);
 }
