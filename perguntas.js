5f2a9b6fee1050c50b9d300146bbc01b0118a0f2
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-11 11:49:21 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -38,42 +38,42 @@ criaCartao('quimica',
     'Como se chama a energia mínima para se chegar ao complexo ativado de uma reação química?', 
     'Energia de Ativação ')
 
-criaCartão('História',
+criaCartao('História',
     'Quais foram os países que participaram do dia D?', 
     'Canadá, EUA, França e Reino Unido')
 
-criaCartão('História', 
+criaCartao('História', 
     'Qual a data de início das Guerras das Coréias?', 
     '25 de junho de 1950')
 
-criaCartão('História',
+criaCartao('História',
      'Quais eram os métodos de comunicação a longa distância dos impérios astecas e incas nas Américas?', 
      'Eles usavam principalmente mensageiros a pé para transportar informações e mensagens de maneira rápida ao longo de rotas bem estabelecidas.')
 
-criaCartão('História', 
+criaCartao('História', 
     'Quem eram os principais nomes da Revolução Francesa?', 
     'Maximilian Robespierre, George Danton, Jean-Paul Marat')
 
-criaCartão('História', 
+criaCartao('História', 
     'Qual foi o estopim para o início da Primeira Guerra Mundial?',
      'Assassinato do Arquiduque Francisco Ferdinando da Áustria')
 
-criaCartão('Física', 
+criaCartao('Física', 
     'Qual o significado da sigla MRUV?',
      'Movimento Retilíneo Uniformemente Variado')
 
-criaCartão('Física',
+criaCartao('Física',
      'O que significa variação gradual?', 
      'Refere-se a uma mudança que ocorre de forma progressiva e contínua')
 
-criaCartão('Física', 
+criaCartao('Física', 
     'Qual a importância das leis de Newton?', 
     'A explicação para o movimento de objetos e a base para dinâmica e previsão de fenômenos físicos')
 
-criaCartão('Física', 
+criaCartao('Física', 
     'Qual é a Terceira Lei de Newton?', 
     'É a lei da ação e reação')
 
-criaCartão('Física', 
+criaCartao('Física', 
     'Quem inventou a corrente alternada?', 
     'Nikola Tesla')
