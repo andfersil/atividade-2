0a6975044be47e27b10c8c5c16f3639d14006ddf
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-11 11:58:41 -0300

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+function criaCartao (categoria, pergunta, resposta) {
+    let conteiner = document.getElementById( 'conteiner')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+<div class="conrteudo-cartao">
+  <h3>categoria</h3>
+  <div class="pergunta-cartao">
+    <p>pergunta</p>
+  </div>
+  <div class="resposta-cartao"></div>
+  <p>pergunta</p>
+</div>`
+}
