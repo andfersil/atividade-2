2d4a361471abb02cd16a1121226606a373b43ceb
andfersil <142528127+andfersil@users.noreply.github.com>
2024-10-11 11:40:18 -0300

    pinto

--- perguntas.js
+++ perguntas.js
@@ -1,43 +1,79 @@
 
 
-criaCartao("matematica", "Qual é a razão trigonometria no triângulo retângulo da função seno?","Cateto oposto / hipotenusa")
+criaCartao('matematica',
+     'Qual é a razão trigonometria no triângulo retângulo da função seno?',
+     'Cateto oposto / hipotenusa')
 
-criaCartao("matematica"," Qual é a fórmula de área de um hexágono regular?", "6L²√3/4")
+criaCartao('matematica',' Qual é a fórmula de área de um hexágono regular?', '6L²√3/4')
 
-criaCartao("matematica"," Como que se calcula o fatorial de um número?","precisamos multiplicar todos os antecessores desse número.
-n X (n-1) X (n-2) X ...
-")
+criaCartao('matematica',
+    ' Como que se calcula o fatorial de um número?',
+    'precisamos multiplicar todos os antecessores desse número.n X (n-1) X (n-2) X ...')
 
-criaCartao("matematica", "Qual a fórmula da área de um trapézio?", "(B+b).h/2")
+criaCartao('matematica', 
+    'Qual a fórmula da área de um trapézio?', 
+    '(B+b).h/2')
 
-criaCartao("matematica", " Qual é a estrutura básica de uma função quadrática ? ", " f(x) = ax²+ bx + c" )
+criaCartao('matematica', 
+    ' Qual é a estrutura básica de uma função quadrática ? ', 
+    ' f(x) = ax²+ bx + c' )
 
-criaCartao("quimica", "Como são denominadas as  4 funções inorgânicas?", " Ácidos, Bases, Sais, Óxidos")
+criaCartao('quimica', 
+    'Como são denominadas as  4 funções inorgânicas?', 
+    ' Ácidos, Bases, Sais, Óxidos')
 
-criaCartao("quimica","Qual é o número atômico do Oxigênio?","16")
+criaCartao('quimica',
+    'Qual é o número atômico do Oxigênio?',
+    '16')
 
-criaCartao("quimica",  "Como se chama a transformação de energia elétrica em  química?", "Eletrólise")
+criaCartao('quimica',  
+    'Como se chama a transformação de energia elétrica em  química?',
+     'Eletrólise')
 
-criaCartao("quimica", "Como são chamadas as estruturas orgânicas que só possuem átomos de carbono e hidrogênio?", "Hidrocarbonetos")
+criaCartao('quimica',
+     'Como são chamadas as estruturas orgânicas que só possuem átomos de carbono e hidrogênio?', 
+     'Hidrocarbonetos')
 
-criaCartao("quimica", "Como se chama a energia mínima para se chegar ao complexo ativado de uma reação química?", "Energia de Ativação ")
+criaCartao('quimica', 
+    'Como se chama a energia mínima para se chegar ao complexo ativado de uma reação química?', 
+    'Energia de Ativação ')
 
-criaCartão('História’, 'Quais foram os países que participaram do dia D?', 'Canadá, EUA, França e Reino Unido')
+criaCartão('História',
+    'Quais foram os países que participaram do dia D?', 
+    'Canadá, EUA, França e Reino Unido')
 
-criaCartão('História', 'Qual a data de início das Guerras das Coréias?', '25 de junho de 1950')
+criaCartão('História', 
+    'Qual a data de início das Guerras das Coréias?', 
+    '25 de junho de 1950')
 
-criaCartão('História', 'Quais eram os métodos de comunicação a longa distância dos impérios astecas e incas nas Américas?', 'Eles usavam principalmente mensageiros a pé para transportar informações e mensagens de maneira rápida ao longo de rotas bem estabelecidas.')
+criaCartão('História',
+     'Quais eram os métodos de comunicação a longa distância dos impérios astecas e incas nas Américas?', 
+     'Eles usavam principalmente mensageiros a pé para transportar informações e mensagens de maneira rápida ao longo de rotas bem estabelecidas.')
 
-criaCartão('História', 'Quem eram os principais nomes da Revolução Francesa?', 'Maximilian Robespierre, George Danton, Jean-Paul Marat')
+criaCartão('História', 
+    'Quem eram os principais nomes da Revolução Francesa?', 
+    'Maximilian Robespierre, George Danton, Jean-Paul Marat')
 
-criaCartão('História', 'Qual foi o estopim para o início da Primeira Guerra Mundial?', 'Assassinato do Arquiduque Francisco Ferdinando da Áustria')
+criaCartão('História', 
+    'Qual foi o estopim para o início da Primeira Guerra Mundial?',
+     'Assassinato do Arquiduque Francisco Ferdinando da Áustria')
 
-criaCartão('Física', 'Qual o significado da sigla MRUV?', 'Movimento Retilíneo Uniformemente Variado')
+criaCartão('Física', 
+    'Qual o significado da sigla MRUV?',
+     'Movimento Retilíneo Uniformemente Variado')
 
-criaCartão('Física', 'O que significa variação gradual?', 'Refere-se a uma mudança que ocorre de forma progressiva e contínua')
+criaCartão('Física',
+     'O que significa variação gradual?', 
+     'Refere-se a uma mudança que ocorre de forma progressiva e contínua')
 
-criaCartão('Física', 'Qual a importância das leis de Newton?', 'A explicação para o movimento de objetos e a base para dinâmica e previsão de fenômenos físicos')
+criaCartão('Física', 
+    'Qual a importância das leis de Newton?', 
+    'A explicação para o movimento de objetos e a base para dinâmica e previsão de fenômenos físicos')
 
-criaCartão('Física', 'Qual é a Terceira Lei de Newton?', 'É a lei da ação e reação')
+criaCartão('Física', 
+    'Qual é a Terceira Lei de Newton?', 
+    'É a lei da ação e reação')
 
-criaCartão('Física', 'Quem inventou a corrente alternada?', 'Nikola Tesla')
+criaCartão('Física', 
+    'Quem inventou a corrente alternada?', 
+    'Nikola Tesla')
