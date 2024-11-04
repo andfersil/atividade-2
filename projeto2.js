70e8448a69ee257b52e62bcb8e149d4a49cf2a20
andfersil <142528127+andfersil@users.noreply.github.com>
2024-11-04 08:25:44 -0300

    ;

--- projeto2.js
+++ projeto2.js
@@ -4,7 +4,7 @@ function criaCartao(categoria,pergunta,resposta){
     let cartao =document.createElement('article');
     cartao.className ='cartao'
     cartao.innerHTML =`
-    <div class="conteudo-cartao" onclick= mostraResposta(this)> 
+    <div class="conteudo-cartao" onclick= mostrarResposta(this)> 
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -20,7 +20,7 @@ function criaCartao(categoria,pergunta,resposta){
 }
 
 
-function mostraResposta(card){
+function mostrarResposta(card){
     const resposta = card.querySelector('.resposta-cartao'); 
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block';
